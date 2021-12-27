4ec8c724b93f77881ca7024b6796d720a8ee3dbd
Koustav Kumar <kkumarr1250@gmail.com>
2021-12-27 19:48:06 +0530

    Increase the security Level to 3

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express")
 const bodyParser=require("body-parser")
 const ejs=require("ejs")
 const mongoose=require("mongoose")
-const encrypt=require("mongoose-encryption");
+// const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express()
 
@@ -20,7 +21,7 @@ const userSchema=new mongoose.Schema({
 })
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 
 const User= mongoose.model("User",userSchema);
@@ -36,13 +37,14 @@ app.get("/login",function(req,res){
 
 app.get("/register",function(req,res){
     res.render("register");
-})
+});
+
 app.post("/register",function(req,res){
     console.log(req.body);
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
-    })
+        password:md5(req.body.password)
+    });
     newUser.save(function(err){
         if(err){
             console.log(err);
@@ -54,7 +56,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     console.log(username+" "+password);
     User.findOne({email:username},function(err,foundUser){
         if(err){
